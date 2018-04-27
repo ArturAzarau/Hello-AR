55b52ea7bb345d3817f2acab414b176bd9a9d066
ArturAzarau <artur.azarau@gmail.com>
2018-04-27 20:37:35 +0300

    Initial Commit

---
+++ Hello-AR/Application Delegate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Hello-AR
+//
+//  Created by Артур Азаров on 27.04.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
